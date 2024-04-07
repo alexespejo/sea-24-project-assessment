71c9e198109ed26449898490ba4a5464319f8fae
Alex Espejo <aespejo102@gmail.com>
2024-04-07 02:58:33 -0700

    created navbar, updated photos, adjusted layout

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
 import { albums_of_all_time } from "./data.js";
 
 // Your final submission should have much more data than this, and
@@ -31,47 +7,120 @@ import { albums_of_all_time } from "./data.js";
 const modalBox = document.getElementById("dialog");
 const modalContent = document.getElementById("modal-content");
 
-function showCards() {
- const cardContainer = document.getElementById("card-container");
- const templateCard = document.querySelector(".card");
- for (let i = 0; i < albums_of_all_time.length; i++) {
-  const nextCard = templateCard.cloneNode(true); // Copy the template card
-  editCardContent(
-   nextCard,
-   i + 1,
-   albums_of_all_time[i].album_title,
-   `./images/${albums_of_all_time[i].cover_url}`,
-   albums_of_all_time[i]
+let savedAlbums = [];
+
+const artistSearchInput = document.getElementById("filterSearch");
+
+let searchArtist = true;
+const searchByArtistBtn = document.getElementById("searchByArtist");
+const searchByAlbumBtn = document.getElementById("searchByAlbum");
+
+function changeSearch(searchingByAlbum) {
+ searchArtist = searchingByAlbum;
+
+ searchByArtistBtn.classList.remove("underline");
+ searchByAlbumBtn.classList.remove("underline");
+ if (searchArtist) {
+  artistSearchInput.placeholder = "Did your Favorite Artist Make the Cut? 👀";
+  searchByArtistBtn.classList.add("underline");
+ } else {
+  artistSearchInput.placeholder = "Did your Favorite Album Make the Cut? 👀";
+  searchByAlbumBtn.classList.add("underline");
+ }
+}
+
+searchByAlbumBtn.addEventListener("click", () => changeSearch(false));
+searchByArtistBtn.addEventListener("click", () => changeSearch(true));
+
+function searchByArtist() {
+ let albums = albums_of_all_time;
+ const searchTerm = artistSearchInput.value.trim().toLowerCase();
+
+ if (searchArtist) {
+  showCards(
+   albums.filter((album) => album.artist.toLowerCase().includes(searchTerm))
   );
-  cardContainer.append(nextCard);
+ } else {
+  showCards(
+   albums.filter((album) =>
+    album.album_title.toLowerCase().includes(searchTerm)
+   )
+  );
+ }
+}
+artistSearchInput.addEventListener("input", () => {
+ searchByArtist();
+});
+
+function showCards(album) {
+ const cardContainer = document.getElementById("card-container");
+ //clear html before each render
+ cardContainer.innerHTML = "";
+
+ const templateCard = document.querySelector(".card");
+ if (album.length === 0) {
+  if (artistSearchInput.value.trim().toLowerCase() === "kanye") {
+   cardContainer.append("lol");
+  } else {
+   cardContainer.append("hmm... Something doesn't seem right 🤔");
+  }
+ } else {
+  for (let i = 0; i < album.length; i++) {
+   const nextCard = templateCard.cloneNode(true); // Copy the template card
+   editCardContent(
+    nextCard,
+    i + 1,
+    album[i].album_title,
+    `./images/${album[i].cover_url ? album[i].cover_url : `emptyImage.svg`}`,
+    album[i]
+   );
+   cardContainer.append(nextCard);
+  }
  }
 }
 
 function editCardContent(card, rank, newTitle, newImageURL, album) {
  card.style.display = "block";
- const rankNumber = card.querySelector(".rank-number");
  const cardHeader = card.querySelector(".title");
  const artistTitle = card.querySelector(".artist");
  const btn = card.querySelector(".btn");
+ const saveBtn = card.querySelector(".btn-save");
 
- rankNumber.textContent = rank;
- cardHeader.textContent = newTitle;
+ cardHeader.textContent = rank + ". " + album.album_title;
  artistTitle.textContent = album.artist;
+ saveBtn.addEventListener("click", () => {
+  if (savedAlbums.map((item) => item.album_title).includes(album.album_title)) {
+   alert("album is already added");
+  } else {
+   savedAlbums.push(album);
+  }
+ });
+
+ //render modal on button click
  btn.addEventListener("click", () => {
   modalContent.innerHTML = `
     <div class="album-info">
       <div class="modal-content-1">
         <h1 class="rank">${rank}. ${album.album_title}</h1>
-        <h3 class="artist">Artist: ${album.artist}</h3>
-        <img src="./images/${album.cover_url}"/>
+       
+        <img src="./images/${
+         album.cover_url ? album.cover_url : `emptyImage.svg`
+        }" alt="${album.album_title} Cover"/>
       </div>
       <div class="modal-content-2">
-        <h3><span class="italic">Genres</span>: ${album.genres.map(
-         (x) => ` ${x}`
-        )}</h3>
-        <h3 class="date">Released: ${album.date}</h3>
-        <h3 class="critic-rating">${album.critic_score}</h3>
-   
+        <h3 class="artist"> <span>Artist:</span> ${album.artist}</h3>
+        <div class="flex">
+          <h3 class="genre-title justify-center">Genres:</h3>
+          <ul class="genre-list">
+            ${album.genres.map((x) => `<li>${x}</li>`).join("")}
+          </ul>
+        </div>
+        
+        <h3 class="date">Released: <span> ${album.date}</span></h3>
+        <h3 class="critic-rating">
+          <span class="label">Critic Score</span> 
+          <span class="score"> ${album.critic_score}</span>
+        </h3>
       </div>
     </div>
   `;
@@ -82,14 +131,13 @@ function editCardContent(card, rank, newTitle, newImageURL, album) {
  cardImage.src = newImageURL;
  cardImage.alt = newTitle + " Poster";
 
- // You can use console.log to help you debug!
- // View the output by right clicking on your website,
- // select "Inspect", then click on the "Console" tab
  console.log("new card:", newTitle, "- html: ", card);
 }
 
 // This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+document.addEventListener("DOMContentLoaded", () => {
+ showCards(albums_of_all_time);
+});
 
 function quoteAlert() {
  console.log("Button Clicked!");
