39276a1447626b809757ad2e1d1441c03c5cb393
Alex Espejo <aespejo102@gmail.com>
2024-04-09 01:25:47 -0700

    final commit

--- scripts.js
+++ scripts.js
@@ -30,8 +30,8 @@ function filterGenre() {
  });
  showCards(displayAlbums);
 }
-
 genreFilterSelectMenue.addEventListener("change", filterGenre);
+
 //Search Bar
 function changeSearch(searchingByAlbum) {
  searchArtist = searchingByAlbum;
